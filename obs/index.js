fa27af0a3e246d0997421000e39b0a15a2c37337
Gabriel Travaini <gabriel.travaini@gmail.com>
2024-10-25 20:13:11 -0300

    added GET and POST routes

--- obs/index.js
+++ obs/index.js
@@ -2,16 +2,21 @@ const express = require('express');
 const app = express();
 app.use(express.json());
 const obsPorLembrete = {};
-const {v4:uuidv4}=require('uuid');
+const { v4: uuidv4 } = require('uuid');
 
 
 
 app.post('/lembretes/:id/obs', (req, res) => {
-
+    const idObs = uuidv4();
+    const { texto } = req.body;
+    const observacoesDoLembrete = obsPorLembrete[req.params.id] || [];
+    observacoesDoLembrete.push({ id: idObs, texto });
+    obsPorLembrete[req.params.id] = observacoesDoLembrete;
+    res.status(201).send(observacoesDoLembrete);
 });
 
 app.get('/lembretes/:id/obs', (req, res) => {
-
+    res.send(obsPorLembrete[req.params.id]||[])
 });
 
 app.listen(5000, (() => {
