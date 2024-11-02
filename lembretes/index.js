4d2fd07ee70434872a9b5fb048d8fb1ad95d2968
Gabriel Travaini <gabriel.travaini@gmail.com>
2024-11-01 21:08:04 -0300

    adjusted services to work with event bridge

--- lembretes/index.js
+++ lembretes/index.js
@@ -1,19 +1,33 @@
 const express = require('express');
+const axios = require('axios')
 const app = express();
 app.use(express.json());
 let lembretes = {};
 let idCounter = 0;
 
-app.get('/lembretes', (req, res) => { 
+app.get('/lembretes', (req, res) => {
     res.send(lembretes);
 })
 
-app.post('/lembretes', (req, res) => { 
+app.post('/lembretes', async (req, res) => {
     idCounter++;
     const { texto } = req.body;
-    lembretes [idCounter] ={idCounter,texto}; 
+    lembretes[idCounter] = { idCounter, texto };
+    await axios.post('http://localhost:10000/eventos', {
+        tipo: "LembreteCriado",
+        dados: {
+            idCounter,
+            texto
+        }
+    })
     res.status(201).send(lembretes[idCounter]);
 })
+
+app.post('/eventos', (req,res)=>{
+    console.log(req.body);
+    res.send({msg:'ok'});
+})
+
 app.listen(4000, () => {
     console.log('Lembretes. Porta 4000');
 });
--- obs/index.js
+++ obs/index.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const axios = require('axios')
 const app = express();
 app.use(express.json());
 const obsPorLembrete = {};
@@ -6,18 +7,29 @@ const { v4: uuidv4 } = require('uuid');
 
 
 
-app.post('/lembretes/:id/obs', (req, res) => {
+app.post('/lembretes/:id/obs', async (req, res) => {
     const idObs = uuidv4();
     const { texto } = req.body;
     const observacoesDoLembrete = obsPorLembrete[req.params.id] || [];
     observacoesDoLembrete.push({ id: idObs, texto });
     obsPorLembrete[req.params.id] = observacoesDoLembrete;
+    await axios.post('http://localhost:10000/eventos', {
+        tipo: "ObservacaoCriada",
+        dados: {
+            id: idObs, texto, lembreteId: req.params.id
+        }
+    })
     res.status(201).send(observacoesDoLembrete);
 });
 
 app.get('/lembretes/:id/obs', (req, res) => {
-    res.send(obsPorLembrete[req.params.id]||[])
+    res.send(obsPorLembrete[req.params.id] || [])
 });
+
+app.post('/eventos', (req,res)=>{
+    console.log(req.body);
+    res.send({msg:'ok'});
+})
 
 app.listen(5000, (() => {
     console.log('obs. Porta 5000');
