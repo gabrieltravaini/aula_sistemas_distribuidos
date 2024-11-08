d2f7fb0ceb39a21ad1fc911b8450861b38c54a88
Gabriel Travaini <gabriel.travaini@gmail.com>
2024-11-08 20:03:25 -0300

    integrates consulta into barramento

--- Barramento/index.js
+++ Barramento/index.js
@@ -8,6 +8,7 @@ app.post('/eventos', (req,res)=> {
     const evento = req.body;
     axios.post('http://localhost:4000/eventos',evento);
     axios.post('http://localhost:5000/eventos',evento);
+    axios.post('http://localhost:6000/eventos',evento)
     res.send({msg:"ok"});
 });
 
